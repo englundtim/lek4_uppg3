752cd915b92b85ea230abffd7ed293ed5e5b144e
Tim <tim.englund@elev.ga.ntig.se>
2023-09-18 08:56:41 +0200

    Skapat prject lek4_uppg3

---
+++ src/App.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Skriv in ditt namn, längd, vikt och ålder");
+        System.out.println("t.ex Eric Saade 156 126 56");
+        
+    }
+}
